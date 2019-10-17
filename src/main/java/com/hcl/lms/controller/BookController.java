dbd6bc26176678997ed7ad112384f811b3057249
Laxman <balajicherlopalli555@gmail.com>
2019-10-17 19:49:23 +0530

    as

--- src/main/java/com/hcl/lms/controller/BookController.java
+++ src/main/java/com/hcl/lms/controller/BookController.java
@@ -3,6 +3,7 @@ package com.hcl.lms.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,6 +25,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RestController
 @RequestMapping("/lms")
+@CrossOrigin(allowedHeaders = { "*", "/" }, origins = { "*", "/" })
 public class BookController {
 
 	@Autowired
